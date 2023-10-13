ad712bba9dc5010d6d1a7dfeef16873b9af730c6
Ahmed BARGADY <bargadyahmed2000@gmail.com>
2023-10-13 12:20:42 +0100

    homepage item in sidebar in docs

---
+++ src/theme/DocSidebar/Desktop/Content/index.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import clsx from "clsx";
+import { ThemeClassNames } from "@docusaurus/theme-common";
+import {
+  useAnnouncementBar,
+  useScrollPosition,
+} from "@docusaurus/theme-common/internal";
+import { translate } from "@docusaurus/Translate";
+import DocSidebarItems from "@theme/DocSidebarItems";
+import styles from "./styles.module.css";
+import Link from "@docusaurus/Link";
+function useShowAnnouncementBar() {
+  const { isActive } = useAnnouncementBar();
+  const [showAnnouncementBar, setShowAnnouncementBar] = useState(isActive);
+  useScrollPosition(
+    ({ scrollY }) => {
+      if (isActive) {
+        setShowAnnouncementBar(scrollY === 0);
+      }
+    },
+    [isActive]
+  );
+  return isActive && showAnnouncementBar;
+}
+export default function DocSidebarDesktopContent({ path, sidebar, className }) {
+  const showAnnouncementBar = useShowAnnouncementBar();
+  return (
+    <nav
+      aria-label={translate({
+        id: "theme.docs.sidebar.navAriaLabel",
+        message: "Docs sidebar",
+        description: "The ARIA label for the sidebar navigation",
+      })}
+      className={clsx(
+        "menu thin-scrollbar",
+        styles.menu,
+        showAnnouncementBar && styles.menuWithAnnouncementBar,
+        className
+      )}>
+      <ul className={clsx(ThemeClassNames.docs.docSidebarMenu, "menu__list")}>
+        <Link class="menu__link menu__link" aria-current="page" to="/">
+          <svg
+            style={{
+              marginRight: "0.5rem",
+            }}
+            viewBox="0 0 24 24"
+            class="breadcrumbHomeIcon_node_modules-@docusaurus-theme-classic-lib-theme-DocBreadcrumbs-Items-Home-styles-module">
+            <path
+              d="M10 19v-5h4v5c0 .55.45 1 1 1h3c.55 0 1-.45 1-1v-7h1.7c.46 0 .68-.57.33-.87L12.67 3.6c-.38-.34-.96-.34-1.34 0l-8.36 7.53c-.34.3-.13.87.33.87H5v7c0 .55.45 1 1 1h3c.55 0 1-.45 1-1z"
+              fill="currentColor"></path>
+          </svg>
+          Home
+        </Link>
+        <DocSidebarItems items={sidebar} activePath={path} level={1} />
+      </ul>
+    </nav>
+  );
+}
