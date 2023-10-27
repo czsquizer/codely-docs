2d8beeaebf27526a9c9d42196c64f0c5c20763f0
Ahmed BARGADY <bargadyahmed2000@gmail.com>
2023-10-27 10:48:17 +0100

    fixed collapse list in the mobile left main sidebar

--- src/theme/Navbar/MobileSidebar/PrimaryMenu/index.js
+++ src/theme/Navbar/MobileSidebar/PrimaryMenu/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useThemeConfig } from "@docusaurus/theme-common";
 import { useNavbarMobileSidebar } from "@docusaurus/theme-common/internal";
 import NavbarItem from "@theme/NavbarItem";
@@ -13,9 +13,17 @@ function useNavbarItems() {
 export default function NavbarMobilePrimaryMenu() {
   const mobileSidebar = useNavbarMobileSidebar();
   const pathname = useLocation();
+  const [isListOpen, setIsListOpen] = useState(false);
+
+  const maxHeight = isListOpen ? "h-auto" : "h-0";
+
+  const toggleList = () => {
+    setIsListOpen(!isListOpen);
+  };
   // TODO how can the order be defined for mobile?
   // Should we allow providing a different list of items?
   const items = useNavbarItems();
+
   return (
     <ul className="menu__list">
       <li className="menu__list-item">
@@ -32,7 +40,7 @@ export default function NavbarMobilePrimaryMenu() {
                 marginRight: "0.5rem",
               }}
               viewBox="0 0 24 24"
-              class="breadcrumbHomeIcon_YNFT breadcrumbHomeIcon_node_modules-@docusaurus-theme-classic-lib-theme-DocBreadcrumbs-Items-Home-styles-module">
+              className="breadcrumbHomeIcon_YNFT breadcrumbHomeIcon_node_modules-@docusaurus-theme-classic-lib-theme-DocBreadcrumbs-Items-Home-styles-module">
               <path
                 d="M10 19v-5h4v5c0 .55.45 1 1 1h3c.55 0 1-.45 1-1v-7h1.7c.46 0 .68-.57.33-.87L12.67 3.6c-.38-.34-.96-.34-1.34 0l-8.36 7.53c-.34.3-.13.87.33.87H5v7c0 .55.45 1 1 1h3c.55 0 1-.45 1-1z"
                 fill="currentColor"></path>
@@ -41,15 +49,99 @@ export default function NavbarMobilePrimaryMenu() {
           Home
         </Link>
       </li>
-      {items.map((item, i) => (
-        <NavbarItem
-          mobile
-          {...item}
-          label={item.label == "Docs" ? "Paid Scripts" : item.label}
-          onClick={() => mobileSidebar.toggle()}
-          key={i}
-        />
-      ))}
+      {items.map(
+        (item, i) =>
+          item.label !== "Docs" && (
+            <NavbarItem
+              mobile
+              {...item}
+              label={item.label}
+              onClick={() => mobileSidebar.toggle()}
+              key={i}
+            />
+          )
+      )}
+
+      {/* custom */}
+      <li
+        className={clsx(
+          isListOpen
+            ? "theme-doc-sidebar-item-category theme-doc-sidebar-item-category-level-1 menu__list-item"
+            : "theme-doc-sidebar-item-category theme-doc-sidebar-item-category-level-1 menu__list-item menu__list-item--collapsed"
+        )}>
+        <div onClick={toggleList} className="menu__list-item-collapsible">
+          <a
+            className={clsx(
+              "menu__link menu__link--sublist menu__link--sublist-caret",
+              isListOpen ? "cst-after-link" : "cst-after-link-not"
+            )}
+            aria-expanded={isListOpen ? "true" : "false"}>
+            Paid Scripts
+          </a>
+        </div>
+        <ul
+          className={clsx(
+            "menu__link list-none cst-list",
+            isListOpen && "list-open"
+          )}>
+          <li className="theme-doc-sidebar-item-link theme-doc-sidebar-item-link-level-2 menu__list-item">
+            <a
+              className="menu__link"
+              tabindex="-1"
+              href="/docs/Paid Scripts/Unijob">
+              Unijob
+            </a>
+          </li>
+          <li className="theme-doc-sidebar-item-link theme-doc-sidebar-item-link-level-2 menu__list-item">
+            <a
+              className="menu__link"
+              tabindex="-1"
+              href="/docs/Paid Scripts/Drugs">
+              Drugs
+            </a>
+          </li>
+          <li className="theme-doc-sidebar-item-link theme-doc-sidebar-item-link-level-2 menu__list-item">
+            <a
+              className="menu__link"
+              tabindex="-1"
+              href="/docs/Paid Scripts/Duty">
+              Duty
+            </a>
+          </li>
+          <li className="theme-doc-sidebar-item-link theme-doc-sidebar-item-link-level-2 menu__list-item">
+            <a
+              className="menu__link"
+              tabindex="-1"
+              href="/docs/Paid Scripts/Robbery">
+              Robbery
+            </a>
+          </li>
+          <li className="theme-doc-sidebar-item-link theme-doc-sidebar-item-link-level-2 menu__list-item">
+            <a
+              className="menu__link"
+              tabindex="-1"
+              href="/docs/Paid Scripts/Anticheat">
+              Anticheat
+            </a>
+          </li>
+          <li className="theme-doc-sidebar-item-link theme-doc-sidebar-item-link-level-2 menu__list-item">
+            <a
+              className="menu__link"
+              tabindex="-1"
+              href="/docs/Paid Scripts/Showrooms">
+              Showrooms
+            </a>
+          </li>
+          <li className="theme-doc-sidebar-item-link theme-doc-sidebar-item-link-level-2 menu__list-item">
+            <a
+              className="menu__link"
+              tabindex="-1"
+              href="/docs/Paid Scripts/Motels">
+              Motels
+            </a>
+          </li>
+        </ul>
+      </li>
     </ul>
   );
 }
